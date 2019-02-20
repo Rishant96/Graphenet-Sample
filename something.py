2add5dd253c57d4f2b1ba973a71d0d586c694f07
Rishant Dutt <rishant.dutt96@gmail.com>
2019-02-20 15:23:11 +0530

    first tutorial complete.

---
+++ something.py
@@ -0,0 +1,17 @@
+import graphene
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(argument=graphene.String(default_value="stranger"))
+
+    def resolve_hello(self, info, argument):
+        return 'Hello ' + argument
+
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute('{ hello }')
+print(result.data['hello'])
+
+result = schema.execute('{ hello (argument: "graph") }')
+print(result.data['hello'])
